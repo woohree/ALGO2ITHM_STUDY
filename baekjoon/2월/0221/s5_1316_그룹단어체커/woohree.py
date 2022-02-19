f09454cf779d12e4f0c4da749f7ac737d1e0a074
woohree <woohree2@gmail.com>
2022-02-19 18:41:07 +0900

    1316, 수정

--- baekjoon/2월/0221/s5_1316_그룹단어체커/woohree.py
+++ baekjoon/2월/0221/s5_1316_그룹단어체커/woohree.py
@@ -8,7 +8,7 @@ def is_group_word(word):
     # 중복된 문자가 나중에 다시 나오는 지 판별할 temp
     # 한번 나온 문자가 나중에 다시 나오면(연속은 상관없음) 그룹 단어가 아님!
     length = len(word)
-    temp = [0] * length
+    temp = []
 
     for idx in range(length - 1):
         # 중복 문자가 다시 나오면 탈출!
@@ -18,7 +18,7 @@ def is_group_word(word):
         else:
             # 다음 문자와 비교해 다른 경우, temp에 현 문자 추가!
             if word[idx] != word[idx+1]:
-                temp[idx] = word[idx]
+                temp.append(word[idx])
 
                 # 마지막 문자 검사
                 if idx == length - 2:
