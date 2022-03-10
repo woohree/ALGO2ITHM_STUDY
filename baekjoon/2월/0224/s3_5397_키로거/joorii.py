d9437c2e98f4122359ce72d5a3947c1551a0e16f
joorii <joori0709@gmail.com>
2022-03-10 22:27:39 +0900

    0224 5397 다시풀기

--- baekjoon/2월/0224/s3_5397_키로거/joorii.py
+++ baekjoon/2월/0224/s3_5397_키로거/joorii.py
@@ -1,41 +1,29 @@
-# 60분 시도 시간초과 실패 ㅜㅜ
+# 60분 시도 시간 초과 실패 ㅜㅜ
 import sys
 sys.stdin = open('M.txt')
 
 
 def key_logger():
-    stack = []
-    idx = -1
+    left = []
+    right = []
 
     for char in text:
-        if char == '<':
-            if idx >= 0:
-                idx -= 1
-        elif char == '>':
-            if idx < len(stack) - 1:
-                idx += 1
-        elif char == '-':
-            if idx >= 0:
-                stack.pop(idx)
-                idx -= 1
-        # 방향키 이외의 글자가 들어왔을 때
-        else:
-            # 가장 마지막에 추가
-            if idx + 1 == len(stack):
-                stack.append(char)
-            # 중간에 추가
-            else:
-                stack.insert(idx + 1, char)
-            idx += 1
+        if char == '<' and left:
+            right.append(left.pop())
+        elif char == '>' and right:
+            left.append(right.pop())
+        elif char == '-' and left:
+            left.pop()
+        # 알파벳 혹은 숫자가 들어 왔을 때
+        elif char.isalpha() or char.isdigit():
+            left.append(char)
 
-    for char in stack:
-        print(char, end='')
-    print()
+    return ''.join(left + right[::-1])
 
 
 T = int(input())
 
-for tc in range(1, T + 1):
-    text = list(map(str, input()))
+for tc in range(T):
+    text = input()
 
-    key_logger()
+    print(key_logger())
