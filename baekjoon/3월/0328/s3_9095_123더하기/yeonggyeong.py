c161b5e269006634d5a637586b0b6466b8555eac
Yeonggyeong <jeljelkim129@gmail.com>
2022-03-25 18:34:07 +0900

    b9095 주석추가

--- baekjoon/3월/0328/s3_9095_123더하기/yeonggyeong.py
+++ baekjoon/3월/0328/s3_9095_123더하기/yeonggyeong.py
@@ -3,10 +3,11 @@ sys.stdin = open('G.txt')
 
 
 def get_count(N):
-
+    # 1, 2, 3까지 구할 수 있는 개수
     dp = [1, 2, 4]
 
     if N >= 4:
+        # 3부터 N까지 
         for i in range(3, N):
             count = dp[i-1] + dp[i-2] + dp[i-3]
             dp.append(count)
