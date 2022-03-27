ed1ec2b6c67658377391bb5dd0379895eb11a3ed
joorii <joori0709@gmail.com>
2022-03-27 15:04:53 +0900

    0328 9095

---
+++ baekjoon/3월/0328/s3_9095_123더하기/joorii.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open('M.txt')
+
+
+def cases(n):
+    dp = [0] * 12       # n은 양수이면 11보다 작다.
+    dp[1], dp[2], dp[3] = 1, 2, 4
+    for i in range(4, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+    return dp[n]
+
+
+T = int(input())
+
+for tc in range(T):
+    n = int(input())
+    print(cases(n))
