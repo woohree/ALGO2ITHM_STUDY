5a4f03d84c09de45ea99b5244e44061843b966c0
qoqls1212 <qoqls1212@gmail.com>
2022-03-28 21:14:38 +0900

    0328

--- baekjoon/2월/0210/s1_15989_1,2,3더하기4/배한빈.py
+++ baekjoon/2월/0210/s1_15989_1,2,3더하기4/배한빈.py
@@ -9,6 +9,17 @@ T = int(input())
 for tc in range(1, T+1):
     n = int(input())
 
+    dp = [[1, 0, 0], [1, 1, 0], [1, 1, 1]]
 
-
-    print(n)
+    if n > 3:
+        i = 4
+        while i <= n:
+            temp = []
+            temp.append(dp[i-2][0])
+            temp.append(dp[i-3][0] + dp[i-3][1])
+            temp.append(dp[i-4][0] + dp[i-4][1] + dp[i-4][2])
+            dp.append(temp)
+            i += 1
+        print(sum(dp[n-1]))
+    else:
+        print(sum(dp[n-1]))
