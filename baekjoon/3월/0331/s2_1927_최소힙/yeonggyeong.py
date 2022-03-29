9ba0b7a211f5a6bfa4a0a29371a00fbca6009107
Yeonggyeong <jeljelkim129@gmail.com>
2022-03-29 14:43:10 +0900

    b1931 b1927 b9375

---
+++ baekjoon/3월/0331/s2_1927_최소힙/yeonggyeong.py
@@ -0,0 +1,23 @@
+import sys
+sys.stdin = open('G.txt')
+import heapq
+
+N = int(sys.stdin.readline())
+
+heap = []
+
+
+for i in range(N):
+    number = int(sys.stdin.readline())
+
+    if number > 0:
+        # heap에 number 넣기
+        heapq.heappush(heap, number)
+    else:
+        # 비어있지 않다면
+        # 가장 작은 값 반환
+        if heap:
+            print(heapq.heappop(heap))
+        # 비어있다면 0 반환
+        else:
+            print(0)
