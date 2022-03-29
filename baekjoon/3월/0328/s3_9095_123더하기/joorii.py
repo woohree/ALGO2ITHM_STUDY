e67f2ed9d517f4abc62e1d7c7fea71b67954408b
joorii <joori0709@gmail.com>
2022-03-29 09:02:00 +0900

    0328 9095 ì

--- baekjoon/3월/0328/s3_9095_123더하기/joorii.py
+++ baekjoon/3월/0328/s3_9095_123더하기/joorii.py
@@ -3,7 +3,7 @@ sys.stdin = open('M.txt')
 
 
 def cases(n):
-    dp = [0] * 12       # n은 양수이면 11보다 작다.
+    dp = [0] * 12       # n은 양수이며 11보다 작다.
     dp[1], dp[2], dp[3] = 1, 2, 4
     for i in range(4, n + 1):
         dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
