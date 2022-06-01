c6305121dc597707e811a1d69e1c1b648082742c
woohree <woohree2@gmail.com>
2022-06-01 16:22:36 +0900

    1504 수정

--- baekjoon/5월/0530 줄세우기 특정한최단경로 뱀 정수삼각형/g4_1504_특정한최단경로/woohree.py
+++ baekjoon/5월/0530 줄세우기 특정한최단경로 뱀 정수삼각형/g4_1504_특정한최단경로/woohree.py
@@ -2,7 +2,7 @@ import sys, heapq
 sys.stdin = open('L.txt')
 
 
-def dijkstra(graph, start, end):    # 그저 다익스트라
+def dijkstra(graph, start):    # 그저 다익스트라
     inf = float('inf')
     D = [inf] * (N+1)
     news = [(0, start)]
@@ -17,7 +17,7 @@ def dijkstra(graph, start, end):    # 그저 다익스트라
                     D[new_v] = new_w
                     heapq.heappush(news, (new_w, new_v))
 
-    return D[end]
+    return D
 
 
 N, E = map(int, input().split())
@@ -28,9 +28,13 @@ for _ in range(E):
     graph[e].append((s, w))
 essential_v = list(map(int, sys.stdin.readline().split()))
 
-# 필수 방문 정점까지 다익스트라 각각 돌리기
-a = dijkstra(graph, 1, essential_v[0]) + dijkstra(graph, essential_v[0], essential_v[1]) + dijkstra(graph, essential_v[1], N)
-b = dijkstra(graph, 1, essential_v[1]) + dijkstra(graph, essential_v[1], essential_v[0]) + dijkstra(graph, essential_v[0], N)
+d1 = dijkstra(graph, 1)                     # 1번 정점부터 모든 정점 최단 거리
+d2 = dijkstra(graph, essential_v[0])        # 필수 정점[0]부터 모든 정점 최단 거리
+d3 = dijkstra(graph, essential_v[1])        # 필수 정점[1]부터 모든 정점 최단 거리
+
+a = d1[essential_v[0]] + d2[essential_v[1]] + d3[N]  # 1 => 필수1 => 필수2 => N
+b = d1[essential_v[1]] + d3[essential_v[0]] + d2[N]  # 1 => 필수2 => 필수1 => N
+
 if a >= float('inf') and b >= float('inf'):  # 경로가 없는 경우(inf값인 경우)
     print(-1)
 else:                                        # 경로가 있는 경우
