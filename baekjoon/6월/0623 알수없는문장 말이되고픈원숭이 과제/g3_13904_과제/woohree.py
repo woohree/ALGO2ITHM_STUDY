2c455df59efb46bccb33d63ed5865e90e7fcacb0
woohree <woohree2@gmail.com>
2022-06-22 15:03:01 +0900

    13904, 수정

--- baekjoon/6월/0623 알수없는문장 말이되고픈원숭이 과제/g3_13904_과제/woohree.py
+++ baekjoon/6월/0623 알수없는문장 말이되고픈원숭이 과제/g3_13904_과제/woohree.py
@@ -6,14 +6,14 @@ N = int(input())
 tasks = []
 for _ in range(N):
     d, w = map(int, sys.stdin.readline().rstrip().split())
-    heapq.heappush(tasks, (-w, d, w))           # 점수 기준 최대 힙
-visited = [0] * 1001                            # 과제를 한 날 확인
+    heapq.heappush(tasks, (-w, d, w))                   # 점수 기준 최대 힙
+visited = [0] * (max(tasks, key=lambda x: x[1])[1]+1)   # 과제 한 날 확인
 ans = 0
 while tasks:
     x, d, w = heapq.heappop(tasks)
-    for i in range(d, 0, -1):                   # 과제 할 수 있는지 확인
-        if not visited[i]:                      # 할 수 있으면, 
-            visited[i] = 1                      # 방문도장 쾅
-            ans += w                            # 점수 갱신
+    for i in range(d, 0, -1):                           # 과제 할 수 있는지 확인
+        if not visited[i]:                              # 할 수 있으면,
+            visited[i] = 1                              # 방문도장 쾅
+            ans += w                                    # 점수 갱신
             break
 print(ans)
