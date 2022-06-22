15801440209dc41729cd7955adf9a504ab57b7fd
woohree <woohree2@gmail.com>
2022-06-22 12:03:14 +0900

    13904, 과제

---
+++ baekjoon/6월/0623 알수없는문장 말이되고픈원숭이 과제/g3_13904_과제/woohree.py
@@ -0,0 +1,19 @@
+import sys, heapq
+sys.stdin = open('L.txt')
+
+
+N = int(input())
+tasks = []
+for _ in range(N):
+    d, w = map(int, sys.stdin.readline().rstrip().split())
+    heapq.heappush(tasks, (-w, d, w))           # 점수 기준 최대 힙
+visited = [0] * 1001                            # 과제를 한 날 확인
+ans = 0
+while tasks:
+    x, d, w = heapq.heappop(tasks)
+    for i in range(d, 0, -1):                   # 과제 할 수 있는지 확인
+        if not visited[i]:                      # 할 수 있으면, 
+            visited[i] = 1                      # 방문도장 쾅
+            ans += w                            # 점수 갱신
+            break
+print(ans)
