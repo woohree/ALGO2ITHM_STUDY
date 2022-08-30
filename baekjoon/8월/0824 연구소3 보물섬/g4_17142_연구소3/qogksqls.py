1ffe6175b7b8c6dea164ebe5c5124888ceddf99d
qoqls1212 <qoqls1212@naver.com>
2022-08-30 22:55:27 +0900

    baehanbeen

--- baekjoon/8월/0824 연구소3 보물섬/g4_17142_연구소3/qogksqls.py
+++ baekjoon/8월/0824 연구소3 보물섬/g4_17142_연구소3/qogksqls.py
@@ -1,11 +1,11 @@
-import sys
+import sys, copy
 from itertools import combinations
 from collections import deque
 
 sys.stdin = open('B.txt')
 
-N, M = map(int, input().split())
-matrix = [list(map(int, input().split())) for _ in range(N)]
+N, M = map(int, sys.stdin.readline().rstrip().split())
+matrix = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(N)]
 
 locations = []
 for i in range(N):
@@ -13,26 +13,41 @@ for i in range(N):
         if matrix[i][j] == 2:
             locations.append((i, j))
 
-
 moves = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-comb = list(combinations(locations, 3))
-my_max = 0
+comb = list(combinations(locations, M))
+my_min, flag = N * N, 0
 for c in comb:
     visited = [[0] * N for _ in range(N)]
+    temp = copy.deepcopy(matrix)
     q = deque(c)
-    time = 0
+    time, ans = 0, 0
+    for a in q:
+        visited[a[0]][a[1]] = 1
+        temp[a[0]][a[1]] = 2
     while q:
+        time += 1
         for _ in range(len(q)):
             now = q.popleft()
-            visited[now[0]][now[1]] = 1
             for move in moves:
                 r = now[0] + move[0]
                 c = now[1] + move[1]
-                if 0 <= r < N and 0 <= c < M:
-                    if visited[r][c] == 0 and matrix[r][c] == 0:
+                if 0 <= r < N and 0 <= c < N:
+                    if visited[r][c] == 0 and temp[r][c] == 2:
                         visited[r][c] = 1
-                        matrix[r][c] = time
                         q.append((r, c))
-        time += 1
-    my_max = max(my_max, time)
-print(my_max)
+                    elif visited[r][c] == 0 and temp[r][c] != 1:
+                        ans = time
+                        visited[r][c] = 1
+                        temp[r][c] = time
+                        q.append((r, c))
+    no = 1
+    for row in temp:
+        if 0 in row:
+            no = 0
+            break
+    if no:
+        my_min = min(my_min, ans)
+if my_min == N * N:
+    print(-1)
+else:
+    print(my_min)
